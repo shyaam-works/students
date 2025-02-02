c75ea36fbe0c28176d2172497da64fbb9c7ced77
Shyaam Sundar <shyaamswork@gmail.com>
2025-02-02 09:54:45 +0530

    final

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
-const jsonServer = require("json-server"); // importing json-server library
-const server = jsonServer.create();
-const router = jsonServer.router("db.json");
-const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 8080; //  chose port from here like 8080, 3001
+const jsonServer = require("json-server"); // Importing json-server
+const cors = require("cors"); // Importing the cors package
 
+const server = jsonServer.create();
+const router = jsonServer.router("db.json"); // Path to your JSON data
+const middlewares = jsonServer.defaults();
+const port = process.env.PORT || 8080; // Set port to either process.env.PORT or 8080
+
+server.use(cors()); // Enable CORS for all routes
 server.use(middlewares);
 server.use(router);
 
-server.listen(port);
+server.listen(port, () => {
+  console.log(`JSON Server is running on port ${port}`);
+});
